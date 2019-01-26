5d4efe1fb33be6a2beca9a46a9615b7448666214
Angel Ancheta <angelancheta98@yahoo.com>
2019-01-26 10:35:50 -0800

    fixed formatting

--- greaterAlgo/greater1.js
+++ greaterAlgo/greater1.js
@@ -7,38 +7,38 @@
 
 
 
-var nextGreaterElement = function(nums, nums2) {
+var nextGreaterElement = function (nums, nums2) {
     //run for loop through first arr
     //indexof to find place of index in second arr
     //for each index ran through for loop compare all next elements to see if they are greater than
     var result = [];
-    for( var i = 0; i < nums.length; i++ ){
+    for (var i = 0; i < nums.length; i++) {
         var compare = nums2.indexOf(nums[i]) + 1
-        if(compare){
+        if (compare) {
             recurs(compare, nums, nums2)
-                function recurs(compare, nums, nums2){
-                        if(nums[i] < nums2[compare]){
-                            result.push(nums2[compare])
-                        }else if(nums[i] < nums2[compare]){
-                            compare++;
-                            recurs(compare, nums, nums2)
-                        }else {
-                            result.push(-1)
-                        }
-                    }
-        }else{
+            function recurs(compare, nums, nums2) {
+                if (nums[i] < nums2[compare]) {
+                    result.push(nums2[compare])
+                } else if (nums[i] < nums2[compare]) {
+                    compare++;
+                    recurs(compare, nums, nums2)
+                } else {
+                    result.push(-1)
+                }
+            }
+        } else {
             result.push(-1)
         }
     }
     return result;
 };
 
-var nums1 = [4,1,2]
-var nums2 = [1,3,4,2]
+var nums1 = [4, 1, 2]
+var nums2 = [1, 3, 4, 2]
 //answer is [-1,3,-1]
 
-var nums3 = [2,4]
-var nums4 = [1,2,3,4]
+var nums3 = [2, 4]
+var nums4 = [1, 2, 3, 4]
 //answer is [3, -1]
 
 nextGreaterElement(nums1, nums2)
